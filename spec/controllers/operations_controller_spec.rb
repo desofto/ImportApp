9d443092e08cfef9601042b8fcdba007bc3f80eb
Dmitry Silchenko <dmitry@desofto.com>
2016-04-02 21:53:48 +0300

    Fix controller test

--- spec/controllers/operations_controller_spec.rb
+++ spec/controllers/operations_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe OperationsController do
   describe '#import' do
     it 'calls OperationsImporter' do
-      expect(OperationsImporter).to receive_message_chain(:new, :import)
+      expect(ImportWorker).to receive(:perform_async).with('test.csv')
 
       allow_any_instance_of(OperationsController).to receive(:uploaded_file_name) { 'test.csv' }
       post :import
